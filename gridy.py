16e17fbaa623b35b7b4fb78c7f0af2eb05f21a0b
Ton Nom <ibrahim.ayyad05@gmail.com>
2025-12-08 00:11:21 +0100

    08/12/2025

--- gridy.py
+++ gridy.py
@@ -8,8 +8,7 @@ class Cellule:
         self.en_cours = False   # pour détecter les cycles
 
 
-def indice_to_colonne(ind):
-    # convertit un indice de colonne (0,1,2,...) en 'A','B',..., 'AA', etc.
+def indice_to_colonne(ind): # convertit un indice (0,1,2,...) en lettre de colonne (A,B,C,...etc)
     resultat = ""
     while True:
         ind, reste = divmod(ind, 26)
@@ -21,7 +20,7 @@ def indice_to_colonne(ind):
     return resultat
 
 
-def creer_dico(fichier):
+def creer_dico(fichier): # crée le dictionnaire à partir du fichier pandas
     nb_lignes, nb_colonnes = fichier.shape
     grille = {}
 
@@ -31,125 +30,103 @@ def creer_dico(fichier):
             numero_ligne = i + 1
             nom_cellule = f"{lettre_colonne}{numero_ligne}"
 
-            texte = fichier.iat[i, j]
-            if pandas.isna(texte):
+            texte = fichier.iat[i, j] # récupère le texte de la cellule [i,j]
+            if pandas.isna(texte): 
                 texte = ""
 
-            cellule = Cellule(str(texte))
-
-            # initialiser la valeur directement si ce n'est pas une formule
-            if not cellule.texte.startswith("="):
-                txt = cellule.texte
-                if txt == "":
-                    cellule.valeur = ""
-                else:
-                    try: # on essaye de convertir en nombre, si erreur alors c'est du texte
-                        cellule.valeur = float(txt)
-                    except ValueError:
-                        cellule.valeur = txt 
-            grille[nom_cellule] = cellule # on ajoute la cellule au dictionnaire
+            grille[nom_cellule] = Cellule(str(texte))
 
     return grille
 
 
-# fonctions pour les formules
-def SUM(*args):
+def SUM(*args): # fonction pour calculer la somme
     return sum(args)
 
 
-def AVG(*args):
+def AVG(*args): # fonction pour calculer la moyenne
     return sum(args) / len(args) if args else 0
 
-def COUNT(*args):
-    return len(args)
 
-#def COUNTA(*args):
+class Eval(dict):
+    def __init__(self, tab):
+        self.tab = tab  # dico des Cellule
+        super().__init__()
+
+        self["SUM"] = SUM
+        self["AVG"] = AVG
+        self["MAX"] = max
+        self["MIN"] = min
+
+    def __getitem__(self, cle):
+
+        # si on a déja la valeur calculée on la renvoie
+        if cle in self:
+            return super().__getitem__(cle)
+
+        # sinon, on récupère la cellule
+        cellule = self.tab[cle]
+
+        # pour détecter si il y a un cycle
+        if cellule.en_cours:
+            cellule.valeur = "#CYCLE"
+            self[cle] = cellule.valeur
+            return cellule.valeur
+
+        cellule.en_cours = True
 
 
-# fonction principale pour évaluer une cellule
+        texte = cellule.texte.strip() # on récupère le texte de la cellule
 
-def evaluer_cellule(nom, dico, valeurs):
-    cellule = dico[nom]
-
-    # pour détecter les cycles
-    if cellule.en_cours:
-        cellule.valeur = "#CYCLE"
-        valeurs[nom] = cellule.valeur
-        return cellule.valeur
-
-    # si on connait déja la valeur, on ne va pas la recalculer
-    if cellule.valeur is not None:
-        return cellule.valeur
-
-
-    if not cellule.texte.startswith("="):     # si c'est pas une formule, c'est soit une chaîne vide, soit une valeur numérique, soit du texte
-        texte = cellule.texte
-        if texte == "":
-            cellule.valeur = ""
-        else: # si ce n'est pas une chaine vide, on vérifie si c'est un nombre
+        if not texte.startswith("="):
+            # si ce n'est pas une formule :
+            if texte == "": # si c'est une chaine vide on laisse
+                val = ""
+            else: # si ce n'est pas une chaine vide :
+                try: # on essaye de convertir en nombre
+                    val = float(texte)
+                except ValueError: # si erreur alors on écrit juste le texte
+                    val = texte
+        else:
+            # si c'est une formule, on enlève le = au début et on évalue
+            formule = texte[1:]
             try:
-                cellule.valeur = float(texte)
-            except ValueError:
-                cellule.valeur = texte
-        # si ce n'est pas un nombre ou une formule, c'est du texte
-        valeurs[nom] = cellule.valeur
-        return cellule.valeur
+                val = eval(formule, self) # on évalue en utilisant le dictionnaire de valeurs comme environnement
+            except Exception: # si l'évaluation échoue il y a une erreur
+                val = "#ERREUR"
 
-    # c'est une formule, on évalue (sauf si il y a une erreur dans la formule, dans ce cas on affiche #ERREUR)
-    cellule.en_cours = True
-    formule = cellule.texte[1:]
-
-    # on évalue la formule en utilisant le dictionnaire des valeurs
-    try:
-        cellule.valeur = eval(formule, valeurs)
-    except Exception:
-        cellule.valeur = "#ERREUR"
-
-    cellule.en_cours = False
-    valeurs[nom] = cellule.valeur # on remplit le dictionnaire des valeurs avec la valeur calculée
-    return cellule.valeur
+        cellule.en_cours = False 
+        cellule.valeur = val
+        self[cle] = val
+        return val
 
 
 
+def calculer_valeurs(dico): # calcule toutes les cellules
+    valeurs = Eval(dico)
+    for cle in dico:
+        valeurs[cle]
 
-
-# fonction pour afficher le dictionnaire de cellules
 
 def afficher_dico(dico):
     affichage = []
 
-    for nom in dico.keys():
+    for nom in sorted(dico.keys()):
         cellule = dico[nom]
         valeur_str = str(cellule.valeur) if cellule.valeur is not None else "None"
-        affichage.append(f"'{nom}' : ('{cellule.texte}', {valeur_str})")
+        line = f"'{nom}' : ('{cellule.texte}', {valeur_str})"
+        affichage.append(line)
 
-    print("----------------------------")
-    print(",\n".join(affichage)) 
-    print("----------------------------")
+    print("--- Affichage du Dictionnaire des Cellules ---")
+    print("{")
+    print(",\n".join(affichage))
+    print("}")
+    print("------------------------------------------")
 
 
 """Programme principal"""
 
-
 fichier = pandas.read_csv("test.csv", header=None)
 dico = creer_dico(fichier)
 
-# on initialise le dictionnaire des valeurs avec les fonctions, qui passera en paramètre à eval
-
-valeurs = {
-    "SUM": SUM,
-    "AVG": AVG,
-    "MAX": max,
-    "MIN": min,
-}
-
-for nom, cellule in dico.items():
-    if cellule.valeur is not None:
-        valeurs[nom] = cellule.valeur
-
-# on rajoute les valeurs déja connues dans le dictionnaire des valeurs
-for nom in dico:
-    evaluer_cellule(nom, dico, valeurs)
-
-
+calculer_valeurs(dico)
 afficher_dico(dico)
